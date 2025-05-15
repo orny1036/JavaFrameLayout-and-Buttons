f7c74e2ba18743406a00de215852560689bcd6ec
nabila2022831036 <ornynabila@gmail.com>
2025-05-15 13:16:26 +0600

    added GridLayOut

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
-        frame.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
-      //  frame.setVisible(true);
+       // frame.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+     frame.setLayout(new GridLayout(3,3, 10, 10));
         JButton button1 = new JButton("1");
         JButton button2 = new JButton("2");
         JButton button3 = new JButton("3");
@@ -18,23 +18,23 @@ public class Main {
         JButton button7 = new JButton("7");
         JButton button8 = new JButton("8");
         JButton button9 = new JButton("9");
-
+/*
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(150, 250));
         panel.setBackground(Color.LIGHT_GRAY);
         panel.setLayout(new FlowLayout());
-        
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
-        panel.add(button4);
-        panel.add(button5);
-        panel.add(button6);
-        panel.add(button7);
-        panel.add(button8);
-        panel.add(button9);
+        */
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(button4);
+        frame.add(button5);
+        frame.add(button6);
+        frame.add(button7);
+        frame.add(button8);
+        frame.add(button9);
 
-        frame.add(panel);
+        //frame.add(panel);
         frame.setVisible(true);
     }
 
